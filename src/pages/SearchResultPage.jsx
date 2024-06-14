f5a00d1675678761332a188dd0ae60517cb8f50e
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-06-14 10:03:49 +0900

    Feat: 검색 결과 페이지 구현

--- src/pages/SearchResultPage.jsx
+++ src/pages/SearchResultPage.jsx
@@ -1,9 +1,12 @@
-import { List, ListItem } from "@mui/material";
+import { Box } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+
 import { getSearchReviews } from "../apis/apis";
 import ReviewItem from "../components/Review/ReviewItem";
 import ReviewList from "../components/Review/ReviewList";
+
+import { ListTitle } from "./HomePage";
 
 export default function SearchResultPage(props) {
   const { searchKeyword } = useParams();
@@ -19,12 +22,28 @@ export default function SearchResultPage(props) {
 
   return (
     <>
-      <h2>검색 결과</h2>
-      <ReviewList reviews={reviews}>
-        {reviews.map((review) => (
-          <ReviewItem review={review} />
-        ))}
-      </ReviewList>
+      <Box
+        sx={{
+          bgcolor: (theme) => `${theme.palette.common.brown200}`,
+          p: "5px 10px",
+          color: "white",
+        }}
+      >
+        <ListTitle>"{searchKeyword}"의 검색 결과</ListTitle>
+      </Box>
+      <Box
+        sx={{
+          border: (theme) => `1px solid ${theme.palette.common.brown100}`,
+          borderRadius: "3px",
+          bgcolor: (theme) => `${theme.palette.common.brown50}`,
+        }}
+      >
+        <ReviewList reviews={reviews}>
+          {reviews.map((review) => (
+            <ReviewItem review={review} />
+          ))}
+        </ReviewList>
+      </Box>
     </>
   );
 }
