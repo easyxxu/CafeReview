02b588483001fc4168c76512f7cd2f73cce37231
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-06-14 09:59:53 +0900

    Feat: Header 구현

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,17 +1,32 @@
-import { Link } from "react-router-dom";
+import { Box, Button } from "@mui/material";
+import CreateOutlinedIcon from "@mui/icons-material/CreateOutlined";
+import FavoriteRoundedIcon from "@mui/icons-material/FavoriteRounded";
+
+import { Link, useNavigate } from "react-router-dom";
 import styled from "@emotion/styled";
 import SearchInput from "../Search/SearchInput";
-import Button from "@mui/material/Button";
-import CreateOutlinedIcon from "@mui/icons-material/CreateOutlined";
 
 export default function Header(props) {
+  const navigate = useNavigate();
+
   return (
     <HeaderWrap>
       <h1>
-        <Link to="/">Java Buzz</Link>
+        <Link to="/">
+          <Box display="flex" alignItems="center">
+            Cafe
+            <FavoriteRoundedIcon sx={{ fontSize: "40px" }} />
+          </Box>
+        </Link>
       </h1>
       <SearchInput />
-      <Button variant="contained" color="brown" size="large" type="button">
+      <Button
+        variant="contained"
+        color="brown"
+        size="large"
+        type="button"
+        onClick={() => navigate("/review/write")}
+      >
         <CreateOutlinedIcon sx={{ mr: "10px" }} fontSize="small" />
         리뷰 작성하기
       </Button>
@@ -23,4 +38,5 @@ const HeaderWrap = styled.header`
   display: flex;
   justify-content: space-between;
   margin: 24px 0;
+  align-items: center;
 `;
